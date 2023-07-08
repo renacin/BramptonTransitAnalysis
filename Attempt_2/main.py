68e058e6e17c492d2da38f8a1f75c4ca64015595
Renacin <renacinm@gmail.com>
2023-07-08 07:46:47 -0400

    How do I get a more accurate measure of speed?

--- Attempt_2/main.py
+++ Attempt_2/main.py
@@ -4,8 +4,10 @@ import sqlite3
 import numpy as np
 from Funcs.func import *
 
-# ------------------------------------------------------------------------------
-# Read In DataBase Using A Sqlite3 Connection & Any Additional Data
+#===============================================================================
+# Step #1: Read In DataBase Using A Sqlite3 Connection & Any Additional Data
+#===============================================================================
+out_path = r"/Users/renacin/Documents/BramptonTransitAnalysis/Attempt_2/Misc/Test_Data.csv"
 db_path = r"/Users/renacin/Documents/BramptonTransitAnalysis/Attempt_2/Data/DataStorage.db"
 bus_stops_csv = r"/Users/renacin/Documents/BramptonTransitAnalysis/Attempt_2/Data/Data_Bus_Stops.csv"
 bus_stops = pd.read_csv(bus_stops_csv)
@@ -45,7 +47,6 @@ RD AS (
 	FROM RawData AS A
 ),
 
-
 -- Step #2: Merge Bus Stop Information Onto Main Table
 WithStopData AS (
 	SELECT
@@ -65,19 +66,21 @@ WithStopData AS (
 	LEFT JOIN BusStops AS B2 ON (A.PRV_STP_ID = B2.stop_id)
 )
 
-
 SELECT *
 FROM WithStopData AS A
 '''
 
-# Pull A Small Subset Of Data For A Certain Bus Route
+# Read SQL Query & Perform Basic Sorting & Duplicate Removal
 transit_df = pd.read_sql_query(sql_query, con)
 con.close()
 transit_df.sort_values(["ID", "ROUTE_ID", "TRIP_ID", "EP_TIME"], inplace=True)
 transit_df.drop_duplicates(inplace=True)
-out_path = r"/Users/renacin/Documents/BramptonTransitAnalysis/Attempt_2/Misc/Test_Data.csv"
 
 
+
+#===============================================================================
+# Step #2: Do Additional Formating Before Column Calculation
+#===============================================================================
 # If Next Stop Is Equal To Previous Stop, Replace With Blank, Foward Fill Next Stop Values & Replace First
 for n_col, p_col in zip(["NXT_STP_ID", "NXT_STP_NAME", "NXT_STP_LAT", "NXT_STP_LONG"], ["PRV_STP_ID", "PRV_STP_NAME", "PRV_STP_LAT", "PRV_STP_LONG"]):
 	transit_df.loc[transit_df[n_col] == transit_df[p_col], p_col] = np.nan
@@ -87,7 +90,9 @@ transit_df = transit_df.drop_duplicates(subset=["ID", "ROUTE_ID", "TRIP_ID", "AV
 
 
 
-# Determine Distance Between Previous Location  & Current Location
+#===============================================================================
+# Step #3: Determine Distance Between, As Well As Speed
+#===============================================================================
 transit_df["DST_PSTP_NXTSTP"] = round(transit_df.apply(lambda x: vec_haversine((x["PRV_STP_LAT"], x["PRV_STP_LONG"]), (x["NXT_STP_LAT"], x["NXT_STP_LONG"])), axis=1), 4)
 transit_df["DST_2_PBSTP"] = round(transit_df.apply(lambda x: vec_haversine((x["PRV_STP_LAT"], x["PRV_STP_LONG"]), (x["C_LAT"], x["C_LONG"])), axis=1), 4)
 
@@ -103,6 +108,9 @@ transit_df["ARV_TME_PBSTP"] = transit_df["EP_TIME"] - transit_df["TME_2_PBSTP"]
 
 
 
+#===============================================================================
+# Step #4: Reorganie Data
+#===============================================================================
 con = sqlite3.connect(":memory:")
 transit_df.to_sql("main_data", con, index=False)
 
@@ -137,21 +145,34 @@ WHERE A.DST_PSTP_NXTSTP > 0
 main_data = pd.read_sql_query(sql_query, con).dropna()
 con.close()
 main_data["TRVL_TIME"] = round((main_data["NXT_STP_TIME"] - main_data["CUR_STP_TIME"]) / 60, 2)
+main_data = main_data[main_data["TRVL_TIME"] > 0]
 
 
+
+#===============================================================================
+# Step #5: Output Results
+#===============================================================================
 main_data = main_data[main_data["SEGMENT_NAME"] == "McMurchy - Zum Steeles Station Stop WB -- TO -- Sheridan College Term - 3/3A/4/4A/11/51/511/104 WB"]
 main_data.to_csv(out_path, index=False)
 
+
+
+
+
+
+
 """
-Most Common Segments:
-McMurchy - Zum Steeles Station Stop WB -- TO -- Sheridan College Term - 3/3A/4/4A/11/51/511/104 WB      536
-Airport Road - Zum Steeles Station Stop WB -- TO -- Torbram - Zum Steeles Station Stop WB               488
-Williams - Zum Main Station Stop SB -- TO -- Vodden - Zum Main Station Stop SB                          392
-Torbram - Zum Steeles Station Stop EB -- TO -- Airport Road - Zum Steeles Station Stop EB               384
-Bramalea - Zum Steeles Station Stop EB -- TO -- Torbram - Zum Steeles Station Stop EB                   383
-County Court South - Zum Main Station Stop NB -- TO -- County Court North - Zum Main Station Stop NB    374
-Torbram - Zum Queen Station Stop WB -- TO -- Glenvale - Zum Queen Station Stop WB                       370
-Vodden - Zum Main Station Stop SB -- TO -- Nelson - Zum Main Station Stop SB                            367
-Bramalea - Zum Steeles Station Stop WB -- TO -- Dixie - Zum Steeles Station Stop WB                     366
-Bovaird - Zum Main Station Stop SB -- TO -- Williams - Zum Main Station Stop SB                         366
+Notes:
+
+	(Most Common Segments)
+	McMurchy - Zum Steeles Station Stop WB -- TO -- Sheridan College Term - 3/3A/4/4A/11/51/511/104 WB      536
+	Airport Road - Zum Steeles Station Stop WB -- TO -- Torbram - Zum Steeles Station Stop WB               488
+	Williams - Zum Main Station Stop SB -- TO -- Vodden - Zum Main Station Stop SB                          392
+	Torbram - Zum Steeles Station Stop EB -- TO -- Airport Road - Zum Steeles Station Stop EB               384
+	Bramalea - Zum Steeles Station Stop EB -- TO -- Torbram - Zum Steeles Station Stop EB                   383
+	County Court South - Zum Main Station Stop NB -- TO -- County Court North - Zum Main Station Stop NB    374
+	Torbram - Zum Queen Station Stop WB -- TO -- Glenvale - Zum Queen Station Stop WB                       370
+	Vodden - Zum Main Station Stop SB -- TO -- Nelson - Zum Main Station Stop SB                            367
+	Bramalea - Zum Steeles Station Stop WB -- TO -- Dixie - Zum Steeles Station Stop WB                     366
+	Bovaird - Zum Main Station Stop SB -- TO -- Williams - Zum Main Station Stop SB                         366
 """
