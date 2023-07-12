ae895c26724842426d8142e9c98e687140d2e488
Renacin <renacinm@gmail.com>
2023-07-12 18:50:44 -0400

    Hmmmmmmmmmm Maybe We Can Use DiffLiB, This May Be Helpful - So Many Cool Built-Ins

--- 2_DataFormatting/collect_route_stops.py
+++ 2_DataFormatting/collect_route_stops.py
@@ -4,27 +4,55 @@ import sqlite3
 import numpy as np
 
 import networkx as nx
+#
+# #===============================================================================
+# # Step #1: Read In Cleaned Data
+# #===============================================================================
+# in_data_path = r"/Users/renacin/Documents/BramptonTransitAnalysis/2_DataFormatting/Misc/Test_Data.csv"
+# segment_data = pd.read_csv(in_data_path)
+#
+# # Focus On One Specific Route
+# segment_data = segment_data[(segment_data["ROUTE_ID"] == '502-295') & (segment_data["U_NAME"].str.contains("Weekday"))]
+# test_df = segment_data.groupby(["U_NAME"], as_index=False).agg(
+# 			FIRST_STP  = ("CUR_STP_NM", "first"),
+# 			LAST_STP   = ("CUR_STP_NM", "last"),
+# 			NUM_STP    = ("CUR_STP_NM", lambda s: len(s)),
+# 			TRVL_DIST  = ("DST_BTW_STPS", lambda s: round(s.sum(), 2)),
+# 			TRVL_TME   = ("TRVL_TIME", lambda s: round(s.sum(), 2)),
+#
+# )
+#
+# test_first = test_df.value_counts("FIRST_STP").rename_axis().reset_index(name='COUNT')
+# print(test_df)
+# print(test_first)
+#
+#
+#
+#
+#
+# stops_from_triplinx = ["City Centre Transit Terminal - Platform L", , "Hurontario St at Eglinton Ave - NB (Zum Main)", "Hurontario St at Bristol Rd - NB (Zum Main)", "Hurontario St at Matheson Blvd - NB (Zum Main)", "Hurontario St at Britannia Rd - NB (Zum Main)", "Hurontario St at Courtneypark Dr - NB (Zum Main)", "Hurontario St at Derry Rd - NB (Zum Main)", "County Court South - Zum Main Station Stop NB", "County Court North - Zum Main Station Stop NB", "Brampton Gateway Terminal Route 502 NB Stop", "Nanwood - Zum Main Station Stop NB", "Wellington - Zum Main Station Stop NB", "Theatre Lane - Zum Main Station Stop NB", "Vodden - Zum Main Station Stop NB", "Williams - Zum Main Station Stop NB", "Bovaird - Zum Main Station Stop NB", "Sandalwood - Zum Main Station Stop NB", "Sandalwood Loop - Route 502 Stop"]
 
-#===============================================================================
-# Step #1: Read In Cleaned Data
-#===============================================================================
-in_data_path = r"/Users/renacin/Documents/BramptonTransitAnalysis/2_DataFormatting/Misc/Test_Data.csv"
-segment_data = pd.read_csv(in_data_path)
 
-# Focus On One Specific Route
-segment_data = segment_data[(segment_data["ROUTE_ID"] == '502-295') & (segment_data["U_NAME"].str.contains("Weekday"))]
-test_df = segment_data.groupby(["U_NAME"], as_index=False).agg(
-			FIRST_STP  = ("CUR_STP_NM", "first"),
-			LAST_STP   = ("CUR_STP_NM", "last"),
-			NUM_STP    = ("CUR_STP_NM", lambda s: len(s)),
-			TRVL_DIST  = ("DST_BTW_STPS", lambda s: round(s.sum(), 2)),
-			TRVL_TME   = ("TRVL_TIME", lambda s: round(s.sum(), 2)),
 
-)
+import difflib
 
-test_first = test_df.value_counts("FIRST_STP").rename_axis().reset_index(name='COUNT')
-print(test_df)
-print(test_first)
+varA = 'plaimountain'
+varS = ['piaimauntain','sky','skymountain','dog','231']
+
+#it parse varB by letters
+best = difflib.get_close_matches(varA, varS)
+print(best[0])
+score = difflib.SequenceMatcher(None, varA, best[0]).ratio()
+print(score)
+
+
+
+
+
+
+
+
+
 
 
 """
