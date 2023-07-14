7c33550e75fcbb525768d053c63ce1e07f5796f0
Renacin <renacinm@gmail.com>
2023-07-14 14:58:13 -0400

    Look into removing common words in bus stop names to exagerate differences in bus stop names

--- 2_DataFormatting/collect_route_stops.py
+++ 2_DataFormatting/collect_route_stops.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import sqlite3
 import numpy as np
-import fuzzywuzzy
+import difflib
 
 
 #===============================================================================
@@ -10,19 +10,30 @@ import fuzzywuzzy
 #===============================================================================
 in_data_path = r"/Users/renacin/Documents/BramptonTransitAnalysis/2_DataFormatting/Misc/Test_Data.csv"
 segment_data = pd.read_csv(in_data_path)
+segment_data["CUR_STP_NM"] = segment_data["CUR_STP_NM"].str.replace(" SB ", " NB ")
 
 # Saved Data
 stops_from_triplinx = ["City Centre Transit Terminal - Platform L", "Hurontario St at Eglinton Ave - NB (Zum Main)", "Hurontario St at Bristol Rd - NB (Zum Main)", "Hurontario St at Matheson Blvd - NB (Zum Main)", "Hurontario St at Britannia Rd - NB (Zum Main)", "Hurontario St at Courtneypark Dr - NB (Zum Main)", "Hurontario St at Derry Rd - NB (Zum Main)", "County Court South - Zum Main Station Stop NB", "County Court North - Zum Main Station Stop NB", "Brampton Gateway Terminal Route 502 NB Stop", "Nanwood - Zum Main Station Stop NB", "Wellington - Zum Main Station Stop NB", "Theatre Lane - Zum Main Station Stop NB", "Vodden - Zum Main Station Stop NB", "Williams - Zum Main Station Stop NB", "Bovaird - Zum Main Station Stop NB", "Sandalwood - Zum Main Station Stop NB", "Sandalwood Loop - Route 502 Stop"]
 
+def match_and_ratio(x, words):
+	try:
+		c_match = difflib.get_close_matches(x, stops_from_triplinx, n=1)[0]
+	except IndexError:
+		c_match = ""
+	match_r = difflib.SequenceMatcher(None, x, c_match).ratio()
+	return c_match, match_r
 
-# # Focus On One Specific Route
-# segment_data = segment_data[(segment_data["ROUTE_ID"] == '502-295') & (segment_data["U_NAME"].str.contains("Weekday"))]
-# segment_data["CLST_NM"] = segment_data["CUR_STP_NM"].apply(lambda x: difflib.get_close_matches(x, stops_from_triplinx)).str[0]
-# segment_data["CLST_SCR"] = segment_data.apply(lambda x: difflib.SequenceMatcher(None, segment_data["CUR_STP_NM"], segment_data["CLST_NM"]).ratio(), axis=1)
-#
-#
-# out_path = r"/Users/renacin/Documents/BramptonTransitAnalysis/2_DataFormatting/Misc/Test_Data.csv"
-# segment_data.to_csv(out_path, index=False)
+# Focus On One Specific Route
+segment_data = segment_data[(segment_data["ROUTE_ID"] == '502-295') & (segment_data["U_NAME"].str.contains("Weekday"))]
+segment_data["STR_MATCH"], segment_data["MATCH_VALUE"] = zip(*(segment_data["CUR_STP_NM"].apply(lambda x: match_and_ratio(x, stops_from_triplinx))))
+segment_data = segment_data[segment_data["MATCH_VALUE"] > 0.96]
+
+plt.hist(segment_data["MATCH_VALUE"], bins=10)
+plt.show()
+
+
+out_path = r"/Users/renacin/Documents/BramptonTransitAnalysis/2_DataFormatting/Misc/Test_Data.csv"
+segment_data.to_csv(out_path, index=False)
 
 """
 Example Routes:
