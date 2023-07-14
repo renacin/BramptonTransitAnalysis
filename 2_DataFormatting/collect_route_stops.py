0da93a6d19abea23bdfa355967364b2e86b996ce
Renacin <renacinm@gmail.com>
2023-07-14 16:29:01 -0400

    Dam, I delete the data I was using to validate it all

--- 2_DataFormatting/collect_route_stops.py
+++ 2_DataFormatting/collect_route_stops.py
@@ -15,6 +15,7 @@ segment_data["CUR_STP_NM"] = segment_data["CUR_STP_NM"].str.replace(" SB ", " NB
 # Saved Data
 stops_from_triplinx = ["City Centre Transit Terminal - Platform L", "Hurontario St at Eglinton Ave - NB (Zum Main)", "Hurontario St at Bristol Rd - NB (Zum Main)", "Hurontario St at Matheson Blvd - NB (Zum Main)", "Hurontario St at Britannia Rd - NB (Zum Main)", "Hurontario St at Courtneypark Dr - NB (Zum Main)", "Hurontario St at Derry Rd - NB (Zum Main)", "County Court South - Zum Main Station Stop NB", "County Court North - Zum Main Station Stop NB", "Brampton Gateway Terminal Route 502 NB Stop", "Nanwood - Zum Main Station Stop NB", "Wellington - Zum Main Station Stop NB", "Theatre Lane - Zum Main Station Stop NB", "Vodden - Zum Main Station Stop NB", "Williams - Zum Main Station Stop NB", "Bovaird - Zum Main Station Stop NB", "Sandalwood - Zum Main Station Stop NB", "Sandalwood Loop - Route 502 Stop"]
 
+
 def match_and_ratio(x, words):
 	try:
 		c_match = difflib.get_close_matches(x, stops_from_triplinx, n=1)[0]
@@ -26,13 +27,13 @@ def match_and_ratio(x, words):
 # Focus On One Specific Route
 segment_data = segment_data[(segment_data["ROUTE_ID"] == '502-295') & (segment_data["U_NAME"].str.contains("Weekday"))]
 segment_data["STR_MATCH"], segment_data["MATCH_VALUE"] = zip(*(segment_data["CUR_STP_NM"].apply(lambda x: match_and_ratio(x, stops_from_triplinx))))
-segment_data = segment_data[segment_data["MATCH_VALUE"] > 0.96]
+segment_data = segment_data[segment_data["MATCH_VALUE"] > 0]
 
 plt.hist(segment_data["MATCH_VALUE"], bins=10)
 plt.show()
 
 
-out_path = r"/Users/renacin/Documents/BramptonTransitAnalysis/2_DataFormatting/Misc/Test_Data.csv"
+out_path = r"/Users/renacin/Documents/BramptonTransitAnalysis/2_DataFormatting/Misc/Debugging.csv"
 segment_data.to_csv(out_path, index=False)
 
 """
