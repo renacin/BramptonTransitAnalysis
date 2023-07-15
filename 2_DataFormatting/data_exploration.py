eb2de59ad8bd821064cdfb2fd6153dfa0de7ecf4
Renacin <renacinm@gmail.com>
2023-07-15 15:02:33 -0400

    Format & clean both tables so we have least amount of data. This new table will show bus stops located at a main bus termimal. Use this to help reduce variance in names

--- 2_DataFormatting/data_exploration.py
+++ 2_DataFormatting/data_exploration.py
@@ -12,3 +12,13 @@ out_path = r"/Users/renacin/Documents/BramptonTransitAnalysis/2_DataFormatting/M
 db_path = r"/Users/renacin/Documents/BramptonTransitAnalysis/2_DataFormatting/Data/DataStorage.db"
 bus_stops_csv = r"/Users/renacin/Documents/BramptonTransitAnalysis/2_DataFormatting/Data/Data_Bus_Stops.csv"
 bus_stops = pd.read_csv(bus_stops_csv)
+
+# Find Bus Stops That Are Located At A Main Terminal, Find The Associated Main Bus Terminal
+# Columns Needed stop_id where value is non-numeric, and parent_station where value is not null
+
+# Find Main Bus Terminals
+main_bus_terminals = bus_stops[~bus_stops["stop_id"].str.isnumeric()]
+
+# Find Bus Stops Located In A Main Bus Terminal
+stops_in_main_terminals = bus_stops[~bus_stops["parent_station"].isnull()]
+print(stops_in_main_terminals)
