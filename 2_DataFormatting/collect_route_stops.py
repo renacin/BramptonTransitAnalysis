2d41f5502f39d853e2f36882f8b59ebc95e218fe
Renacin <renacinm@gmail.com>
2023-07-14 20:11:57 -0400

    Splitting by dash, data is too good to be true, I need to think about how to do this

--- 2_DataFormatting/collect_route_stops.py
+++ 2_DataFormatting/collect_route_stops.py
@@ -8,12 +8,16 @@ import difflib
 #===============================================================================
 # Step #1: Read In Cleaned Data
 #===============================================================================
-in_data_path = r"/Users/renacin/Documents/BramptonTransitAnalysis/2_DataFormatting/Misc/Test_Data.csv"
+in_data_path = r"/Users/renacin/Documents/BramptonTransitAnalysis/2_DataFormatting/Misc/main_data.csv"
 segment_data = pd.read_csv(in_data_path)
 segment_data["CUR_STP_NM"] = segment_data["CUR_STP_NM"].str.replace(" SB ", " NB ")
 
 # Saved Data
 stops_from_triplinx = ["City Centre Transit Terminal - Platform L", "Hurontario St at Eglinton Ave - NB (Zum Main)", "Hurontario St at Bristol Rd - NB (Zum Main)", "Hurontario St at Matheson Blvd - NB (Zum Main)", "Hurontario St at Britannia Rd - NB (Zum Main)", "Hurontario St at Courtneypark Dr - NB (Zum Main)", "Hurontario St at Derry Rd - NB (Zum Main)", "County Court South - Zum Main Station Stop NB", "County Court North - Zum Main Station Stop NB", "Brampton Gateway Terminal Route 502 NB Stop", "Nanwood - Zum Main Station Stop NB", "Wellington - Zum Main Station Stop NB", "Theatre Lane - Zum Main Station Stop NB", "Vodden - Zum Main Station Stop NB", "Williams - Zum Main Station Stop NB", "Bovaird - Zum Main Station Stop NB", "Sandalwood - Zum Main Station Stop NB", "Sandalwood Loop - Route 502 Stop"]
+
+# Get Rid Of Unneeded Characters
+stops_from_triplinx = [x.split(" - ")[0] for x in stops_from_triplinx]
+segment_data["CUR_STP_NM"] = segment_data["CUR_STP_NM"].str.split(" - ").str[0]
 
 
 def match_and_ratio(x, words):
@@ -27,13 +31,13 @@ def match_and_ratio(x, words):
 # Focus On One Specific Route
 segment_data = segment_data[(segment_data["ROUTE_ID"] == '502-295') & (segment_data["U_NAME"].str.contains("Weekday"))]
 segment_data["STR_MATCH"], segment_data["MATCH_VALUE"] = zip(*(segment_data["CUR_STP_NM"].apply(lambda x: match_and_ratio(x, stops_from_triplinx))))
-segment_data = segment_data[segment_data["MATCH_VALUE"] > 0]
+segment_data = segment_data[segment_data["MATCH_VALUE"] > 0.0]
 
 plt.hist(segment_data["MATCH_VALUE"], bins=10)
 plt.show()
 
 
-out_path = r"/Users/renacin/Documents/BramptonTransitAnalysis/2_DataFormatting/Misc/Debugging.csv"
+out_path = r"/Users/renacin/Documents/BramptonTransitAnalysis/2_DataFormatting/Misc/Test_Data.csv"
 segment_data.to_csv(out_path, index=False)
 
 """
--- 2_DataFormatting/format_data.py
+++ 2_DataFormatting/format_data.py
@@ -8,7 +8,7 @@ from Funcs.func import *
 #===============================================================================
 # Step #1: Read In DataBase Using A Sqlite3 Connection & Any Additional Data
 #===============================================================================
-out_path = r"/Users/renacin/Documents/BramptonTransitAnalysis/2_DataFormatting/Misc/Test_Data.csv"
+out_path = r"/Users/renacin/Documents/BramptonTransitAnalysis/2_DataFormatting/Misc/main_data.csv"
 db_path = r"/Users/renacin/Documents/BramptonTransitAnalysis/2_DataFormatting/Data/DataStorage.db"
 bus_stops_csv = r"/Users/renacin/Documents/BramptonTransitAnalysis/2_DataFormatting/Data/Data_Bus_Stops.csv"
 bus_stops = pd.read_csv(bus_stops_csv)
