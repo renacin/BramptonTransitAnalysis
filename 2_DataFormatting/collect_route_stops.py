2420b5a56b06fde375a1409cc54c0bece9bcf3e4
Renacin <renacinm@gmail.com>
2023-07-22 14:34:38 -0400

    How do I do this?

--- 2_DataFormatting/collect_route_stops.py
+++ 2_DataFormatting/collect_route_stops.py
@@ -6,37 +6,16 @@ import difflib
 
 
 #===============================================================================
-# Step #1: Read In Cleaned Data
+# Step #1: Read In Bus Stops Within A Given Route Segment
 #===============================================================================
-in_data_path = r"/Users/renacin/Documents/BramptonTransitAnalysis/2_DataFormatting/Misc/main_data.csv"
+in_data_path = r"/Users/renacin/Documents/BramptonTransitAnalysis/2_DataFormatting/Data/185_501_BusStops.csv"
 segment_data = pd.read_csv(in_data_path)
 
-# Saved Data
-stops_from_triplinx = ["City Centre Transit Terminal - Platform L", "Hurontario St at Eglinton Ave - NB (Zum Main)", "Hurontario St at Bristol Rd - NB (Zum Main)", "Hurontario St at Matheson Blvd - NB (Zum Main)", "Hurontario St at Britannia Rd - NB (Zum Main)", "Hurontario St at Courtneypark Dr - NB (Zum Main)", "Hurontario St at Derry Rd - NB (Zum Main)", "County Court South - Zum Main Station Stop NB", "County Court North - Zum Main Station Stop NB", "Brampton Gateway Terminal Route 502 NB Stop", "Nanwood - Zum Main Station Stop NB", "Wellington - Zum Main Station Stop NB", "Theatre Lane - Zum Main Station Stop NB", "Vodden - Zum Main Station Stop NB", "Williams - Zum Main Station Stop NB", "Bovaird - Zum Main Station Stop NB", "Sandalwood - Zum Main Station Stop NB", "Sandalwood Loop - Route 502 Stop"]
-
-# Split By Dash
-stops_from_triplinx = [x.split(" - ")[0] for x in stops_from_triplinx]
-segment_data["CUR_STP_NM"] = segment_data["CUR_STP_NM"].str.split(" - ").str[0]
-
-def match_and_ratio(x, words):
-	try:
-		c_match = difflib.get_close_matches(x, stops_from_triplinx, n=1)[0]
-	except IndexError:
-		c_match = ""
-	match_r = difflib.SequenceMatcher(None, x, c_match).ratio()
-	return c_match, match_r
-
-# Focus On One Specific Route
-segment_data = segment_data[(segment_data["ROUTE_ID"] == '502-295') & (segment_data["U_NAME"].str.contains("Weekday"))]
-segment_data["STR_MATCH"], segment_data["MATCH_VALUE"] = zip(*(segment_data["CUR_STP_NM"].apply(lambda x: match_and_ratio(x, stops_from_triplinx))))
-segment_data = segment_data[segment_data["MATCH_VALUE"] > 0.0]
-
-plt.hist(segment_data["MATCH_VALUE"], bins=10)
-plt.show()
 
 
-out_path = r"/Users/renacin/Documents/BramptonTransitAnalysis/2_DataFormatting/Misc/Test_Data.csv"
-segment_data.to_csv(out_path, index=False)
+
+
+
 
 """
 Example Routes:
