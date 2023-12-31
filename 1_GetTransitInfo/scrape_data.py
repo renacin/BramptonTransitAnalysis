f7e25a2dd4957d40f4a41e8097bced6d3c49dd7e
Renacin <renacinm@gmail.com>
2023-12-31 10:49:39 -0500

    So no numbers match, but the stop names seem to be matching. I need to create a program that pulls all stops in a given route, and checks to see if that bus stop name is in Brmpton Transit's data repository. If it is, I can go about this that way

--- 1_GetTransitInfo/scrape_data.py
+++ 1_GetTransitInfo/scrape_data.py
@@ -9,19 +9,19 @@ from bs4 import BeautifulSoup
 # ---------------------------------------------------------------------------------------------------------------------
 
 
-def pull_routes(transit_url):
+def get_rt_info(transit_url):
 	"""
-	This function navigates to Brampton Transit's Routes & Map Page.
-	It parses all hrefs related to routes - including variations of a given route.
-	It returns a pandas dataframe with the route name, direction, and link.
+	Given a URL, thi function navigates to Brampton Transit's Routes & Map Page,
+	parses all hrefs related to routes, and returns a pandas dataframe with the
+	scraped data.
 	"""
 
-	# Navigate To WebPage
+	# Navigate To WebPage & Grab HTML Data
 	page = requests.get(transit_url)
+	soup = BeautifulSoup(page.content, "html.parser")
 
 	# Parse All HTML Data, Find All HREF Tags
 	rt_data = []
-	soup = BeautifulSoup(page.content, "html.parser")
 	for tag in soup.find_all('a', href=True):
 		str_ref = str(tag)
 		if "https://www.triplinx.ca/en/route-schedules/6/RouteSchedules/" in str_ref:
@@ -38,10 +38,42 @@ def pull_routes(transit_url):
 	return pd.DataFrame(rt_data, columns=["RT_LINK", "RT_DIR", "RT_GRP", "RT_NUM", "RT_NM"])
 
 
+
+def get_rt_stops(rt_list):
+	"""
+	Given a list of links, this function navigates through each, pulling information
+	regarding each bus stop visited. This function returns a pandas dataframe.
+	"""
+
+	for rt in rt_list:
+		# Navigate To WebPage
+		page = requests.get(rt)
+		soup = BeautifulSoup(page.content, "html.parser")
+
+		# Parse All HTML Data, Find All HREF Tags
+		for tag in soup.find_all('a', href=True):
+			str_ref = str(tag)
+			if "StopSchedules/" in str_ref:
+				print(str_ref)
+
+		break
+
+
+"""
+
+<a class="link-to-stop"
+href="/en/stop-schedules/28/StopSchedules/smart-vmc-terminal-route-501-zum-queen-wb/53762/zum-queen/510/501-zum-queen-eastbound-501c-zum-queen-east-407/1?Date=12%2F31%2F2023&amp;
+PhysicalId=807115">Smart VMC Terminal - Route 501 Zum Queen WB</a>
+
+"""
+
+
+
+
 # ---------------------------------------------------------------------------------------------------------------------
 def main():
-	rt_df = pull_routes("https://www1.brampton.ca/EN/residents/transit/plan-your-trip/Pages/Schedules-and-Maps.aspx")
-
+	rt_df = get_rt_info("https://www1.brampton.ca/EN/residents/transit/plan-your-trip/Pages/Schedules-and-Maps.aspx")
+	get_rt_stops(rt_df["RT_LINK"].to_list())
 
 
 # ---------------------------------------------------------------------------------------------------------------------
