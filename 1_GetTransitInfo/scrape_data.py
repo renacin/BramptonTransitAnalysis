0cee86b329460337bc249abbb31a52a515392bc2
Renacin <renacinm@gmail.com>
2023-12-31 10:11:45 -0500

    Why is it so hard to find which bus stops are in a given route? I don't want to use a GIS as it breaks the flow, and won't easily be repeatable

--- 1_GetTransitInfo/scrape_data.py
+++ 1_GetTransitInfo/scrape_data.py
@@ -2,19 +2,25 @@
 # Date:                                               12/29/2023
 # Title                                Gather Transit Route Info, Stops In A Route
 #
-# ----------------------------------------------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------------------------------------
 import requests
+import pandas as pd
 from bs4 import BeautifulSoup
-# ----------------------------------------------------------------------------------------------------------------------
-def main():
-	""" This function will define the main logic of the data exploration section of this project """
+# ---------------------------------------------------------------------------------------------------------------------
+
+
+def pull_routes(transit_url):
+	"""
+	This function navigates to Brampton Transit's Routes & Map Page.
+	It parses all hrefs related to routes - including variations of a given route.
+	It returns a pandas dataframe with the route name, direction, and link.
+	"""
 
 	# Navigate To WebPage
-	URL = "https://www1.brampton.ca/EN/residents/transit/plan-your-trip/Pages/Schedules-and-Maps.aspx"
-	page = requests.get(URL)
+	page = requests.get(transit_url)
 
 	# Parse All HTML Data, Find All HREF Tags
-	rt_lk, rt_dir= [], []
+	rt_data = []
 	soup = BeautifulSoup(page.content, "html.parser")
 	for tag in soup.find_all('a', href=True):
 		str_ref = str(tag)
@@ -22,25 +28,24 @@ def main():
 			for var in ['<a href="', '</a>']:
 				str_ref = str_ref.replace(var, "")
 
-			# Parse Out Route Link As Well As Direction
+			# Parse Out Route Name, Direction, Group, Link, Etc...
 			raw_link, dir = str_ref.split('">')
 			link = raw_link.split('?')[0]
+			full_data = [link + "#trips", dir] + link.split("/")[7:10]
+			rt_data.append(full_data)
 
-			# Append Data To Lists
-			rt_lk.append(link + "#trips")
-			rt_dir.append(dir)
-
-	print(rt_lk)
+	# Return A Pandas Dataframe With Route Data
+	return pd.DataFrame(rt_data, columns=["RT_LINK", "RT_DIR", "RT_GRP", "RT_NUM", "RT_NM"])
 
 
-# ----------------------------------------------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------------------------------------
+def main():
+	rt_df = pull_routes("https://www1.brampton.ca/EN/residents/transit/plan-your-trip/Pages/Schedules-and-Maps.aspx")
+
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+
 # Main Entry Point Into Python Code
 if __name__ == "__main__":
     main()
-
-
-"""
-
-https://www.triplinx.ca/en/route-schedules/6/RouteSchedules/zum-queen/510/501-zum-queen-eastbound-hwy-7-at-eb-vaughan-metrop/1
-
-"""
