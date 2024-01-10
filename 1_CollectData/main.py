7281c3fd8061dcab774e70f0e6c84baa5b972899
Renacin <renacinm@gmail.com>
2024-01-10 11:30:22 -0500

    Class is working, now fill it

--- 1_CollectData/main.py
+++ 1_CollectData/main.py
@@ -3,34 +3,20 @@
 # Title                                   Brampton Transit But Location Parse
 #
 # ----------------------------------------------------------------------------------------------------------------------
-from Funcs.webcrawler import *
+from Funcs.data_collector import *
 # ----------------------------------------------------------------------------------------------------------------------
 
 
-def collect_data():
-    """ This function will define the main logic of this data collection experiment """
+def main():
+	""" This function will define the main logic of this data collection experiment """
 
-    # Instantiate webcrawler, and connect to databse
-    Crawler = WebCrawler()
-    SQLite_DB = SQLite_Database(r"DataStorage.db")
-
-    while True:
-        # Constantly gather from JSON stream | Every 30 seconds?
-        raw_data = Crawler.gather_transit_data()
-        cleaned_data = Crawler.clean_transit_data(raw_data)
-
-        # Append Data To Database
-        SQLite_DB.addtoDB(cleaned_data, 1)
-
-        # Wait 30 Seconds
-        time.sleep(30)
+	# Instantiate webcrawler, and connect to databse
+	Collector = DataCollector()
 
 
-ß
+
 # ----------------------------------------------------------------------------------------------------------------------
 
 # Main Entry Point Into Python Code
 if __name__ == "__main__":
-
-	# Collect Data & Append To Database
-	collect_data()
+	main()
