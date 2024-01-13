efd88cfc6e8280be70cb0b0990215dac585b4dc1
Renacin <renacinm@gmail.com>
2024-01-13 15:11:31 -0500

    Slowly making progress

--- 1_CollectData/Funcs/data_collector.py
+++ 1_CollectData/Funcs/data_collector.py
@@ -25,22 +25,26 @@ class DataCollector:
 		self.bus_routes_url = r"https://www1.brampton.ca/EN/residents/transit/plan-your-trip/Pages/Schedules-and-Maps.aspx"
 		self.bus_stops_url = r"https://opendata.arcgis.com/api/v3/datasets/1c9869fd805e45339a9e3373fc10ce08_0/downloads/data?format=csv&spatialRefId=3857&where=1%3D1"
 
+		# Create A Connection To The Database
+		self.conn = sqlite3.connect(db_path)
+
 		# Given Path, Make Sure Needed Database Exists, Establish Connection To DB
-		self.conn =	self.__db_check(db_path)
+		self.__db_check()
 
-		# Check To See If Data On Routes Has Been Collected, If It Does Compare, Keep Bigger List
-		# TODO
+		# Grab The Most Recent Bus Stop Data From Brampton Open Data
+		self.__get_bus_stops()
 
-	# -------------------------- Private Functions ----------------------------
-	def __db_check(self, db_path):
+
+
+	# -------------------------- Private Functions -----------------------------
+	def __db_check(self):
 		""" On instantiation this function will be called. Create a database
 		that will store bus location data. This is a private function. It cannot
 		be called."""
 
 		# Connect to database check if it has data in it | Create If Not There
 		try:
-			conn = sqlite3.connect(db_path)
-			conn.execute(
+			self.conn.execute(
 			'''
 			CREATE TABLE IF NOT EXISTS BUS_LOC_DB (
 			id                    TEXT,
@@ -69,10 +73,28 @@ class DataCollector:
 			);
 			''')
 
-			return conn
-
 		except sqlite3.OperationalError as e:
 			print(e)
+
+
+	# --------------------------------------------------------------------------
+	def __get_bus_stops(self):
+		""" On instantiation this function will be called. Using Brampton's Open
+		Data API Link, Download, Bus Stop Data To SQLite3 Database. This function
+		should only be run on instantiation. """
+
+		# Compare Data From Bus Stops Collected And Brampton Bus Stop Dataset. Which Are Missing?
+		dwnld_stp_data_df = pd.read_csv(self.bus_stops_url)
+		dwnld_stp_data_df.to_sql("BUS_STP_DB", self.conn, if_exists="replace", index=False)
+
+
+	# --------------------------------------------------------------------------
+
+
+
+
+
+
 
 
 # ----------------------------------------------------------------------------------------------------------------------
@@ -80,16 +102,9 @@ class DataCollector:
 # Main Entry Point Into Python Code
 if __name__ == "__main__":
 
-	# Create A Instance Of The Data Collector
+	# Dine Paths Needed
 	out_path = r"/Users/renacin/Documents/BramptonTransitAnalysis/3_Data"
 	db_path = out_path + "/DataStorage.db"
+
+	# Create An Instance Of The Data Collector
 	Collector = DataCollector(db_path)
-
-
-	"""
-	Order of operations for data collector:
-	1) Instantiate Needed Variables
-	2) On Instantiation Of Class, Given A Path, Create, Or Check Existence Of Database
-	3) On Instantiation Get Most Recent Version Of Routes, And Bus Stops, Should Only Do This Once
-
-	"""
