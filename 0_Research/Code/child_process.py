09688a04cb13dd7af52eef8eeeeef6402f34de4b
Renacin <renacinm@gmail.com>
2024-11-02 14:20:56 -0400

    Swith over to DataCollection branch

--- 0_Research/Code/child_process.py
+++ 0_Research/Code/child_process.py
@@ -1,34 +1,15 @@
-# Name:                                            Renacin Matadeen
-# Date:                                               01/10/2024
-# Title                                 Do Child Threads Still Hold Onto Memory?
-#
-# ----------------------------------------------------------------------------------------------------------------------
-import time
-from concurrent.futures import ThreadPoolExecutor
-# ----------------------------------------------------------------------------------------------------------------------
+# # Name:                                            Renacin Matadeen
+# # Date:                                               01/10/2024
+# # Title                                 Do Child Threads Still Hold Onto Memory?
+# #
+# # ----------------------------------------------------------------------------------------------------------------------
+from time import sleep
+from concurrent.futures import ProcessPoolExecutor
+# # ----------------------------------------------------------------------------------------------------------------------
 
-# for var, obj in list(locals().items()):
-#     print(var, sys.getsizeof(obj))
+def task():
+    sleep(1)
 
-# Define Function
-def test_func():
-	test_list = [x for x in range(100_000_000)]
-
-
-# # Continuously Loop To See If Memory Decreases
-# while True:
-#
-# 	with ThreadPoolExecutor(max_workers=1) as executor:
-# 		result = executor.submit(test_func).result()
-# 		# executor.submit(test_func)
-#
-# 	time.sleep(10)
-
-
-
-
-# Continuously Loop To See If Memory Decreases
-while True:
-
-	test_func()
-	time.sleep(10)
+if __name__ == '__main__':
+    with ProcessPoolExecutor() as exe:
+        exe.submit(task)
