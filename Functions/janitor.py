8fe3404f048ee45a05d3447e6b9484ced86e4a00
RenacinM_Desktop <renacinm@gmail.com>
2026-03-27 22:39:38 -0400

    Getting close to pulling route data

--- Functions/janitor.py
+++ Functions/janitor.py
@@ -29,18 +29,27 @@ class Janitor:
 
 
     # -------------------- Functions Run On Instantiation ----------------------
-    def __init__(self, skp_dwnld=False):
+    def __init__(self):
         """ This function will run when the DataCollector Class is instantiated """
+
+        # Internalize Needed URLs: Bus Location API, Bus Routes, Bus Stops
+        self.bus_routes_url = r"https://www1.brampton.ca/EN/residents/transit/plan-your-trip/Pages/Schedules-and-Maps.aspx"
+        self.bus_stops_url  = r"https://opendata.arcgis.com/api/v3/datasets/1c9869fd805e45339a9e3373fc10ce08_0/downloads/data?format=csv&spatialRefId=3857&where=1%3D1"
+        
+        # Check To See If Appropriate Sub Folders Exist, Where Are We Writting Data?
+        self.out_dict = {}
 
         # Datetime Variables
         self.td_l_dt_dsply_frmt = "%d-%m-%Y %H:%M:%S"
         self.td_s_dt_dsply_frmt = "%d-%m-%Y"
 
+
         # Find What Operating System And Create A Temp Working Space In Downloads Folder
         self.__find_op_sys()
         self.__find_downloads_folder()
         self.__define_paths()
-
+        self.__create_folders()
+        self.__db_check()
 
 
     # -------------------- Private Function #1 ---------------------------------
@@ -74,7 +83,7 @@ class Janitor:
 
     # -------------------- Private Function #3 ---------------------------------
     def __define_paths(self):
-        """ Given The Validity Of Downloads Folder Define Needed Paths For Each Folder (OS Specific)"""
+        """ Given The Validity Of Downloads Folder Define Needed Paths For Each Folder (OS Specific) """
 
         # If *UNIX Based
         if self.op_sys in ["MacOS", "Linux"]:
@@ -98,6 +107,92 @@ class Janitor:
             sys.exit(1)
 
 
+
+    # -------------------- Private Function #4 ---------------------------------
+    def __create_folders(self):
+        """ Create Needed Folders If They Don't Exist Already """
+
+        # First Check To See If The Main Folder Exists!
+        if not os.path.isdir(self.db_folder):
+            os.makedirs(self.db_folder)
+
+        # In The Out Directory Provided See If The Appropriate Sub Folders Exist!
+        for fldr_nm in ['BUS_STP', 'BUS_LOC', 'FRMTD_DATA', 'BUS_SPEED', 'GRAPHICS', 'ERROR']:
+            dir_chk = f"{self.csv_out_path}/{fldr_nm}"
+            self.out_dict[fldr_nm] = dir_chk 
+            if not os.path.exists(dir_chk):
+                os.makedirs(dir_chk)
+
+
+
+    # -------------------------- Private Function 5 ----------------------------
+    def __db_check(self):
+        """ Create a database that will store bus location data; as well as basic database inter data """
+
+        # Connect to database check if it has data in it | Create If Not There
+        try:
+            conn = sqlite3.connect(self.db_path)
+            conn.execute(
+            '''
+            CREATE TABLE IF NOT EXISTS BUS_LOC_DB (
+            u_id                  TEXT,
+            id                    TEXT, is_deleted            TEXT, trip_update           TEXT,
+            alert                 TEXT, trip_id               TEXT, start_time            TEXT,
+            start_date            TEXT, schedule_relationship TEXT, route_id              TEXT,
+            latitude              TEXT, longitude             TEXT, bearing               TEXT,
+            odometer              TEXT, speed                 TEXT, current_stop_sequence TEXT,
+            current_status        TEXT, timestamp             TEXT, congestion_level      TEXT,
+            stop_id               TEXT, vehicle_id            TEXT, label                 TEXT,
+            license_plate         TEXT, dt_colc               TEXT);
+            ''')
+
+            conn.commit()
+            conn.close()
+
+        except sqlite3.OperationalError as e:
+            print(e)
+            sys.exit(1)
+
+
+
+        # Connect to database check if it has data in it | Create If Not There
+        try:
+            conn = sqlite3.connect(self.db_path)
+            conn.execute(
+            '''
+            CREATE TABLE IF NOT EXISTS U_ID_TEMP (
+            u_id                    TEXT,
+            timestamp               TEXT
+            );
+            ''')
+
+            conn.commit()
+            conn.close()
+
+        except sqlite3.OperationalError as e:
+            print(e)
+            sys.exit(1)
+
+
+
+        # Connect to database check if it has data in it | Create If Not There
+        try:
+            conn = sqlite3.connect(self.db_path)
+            conn.execute(
+            '''
+            CREATE TABLE IF NOT EXISTS ERROR_DB (
+            timestamp               TEXT,
+            e_type                  TEXT,
+            delay                   TEXT
+            );
+            ''')
+
+            conn.commit()
+            conn.close()
+
+        except sqlite3.OperationalError as e:
+            print(e)
+            sys.exit(1)
 
 
 
