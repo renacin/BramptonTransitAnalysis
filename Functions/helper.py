e7d61e67f51b1291a6b735c38d32a6b04d964320
RenacinM_Desktop <renacinm@gmail.com>
2026-05-29 10:35:15 -0400

    Redoing the Logging Function, Don't Log To Log Files, Instead Log To A Database, It Allows For Concurrent Writes

--- Functions/helper.py
+++ Functions/helper.py
@@ -69,6 +69,9 @@ def shared_logger(logger_name="", message_txt="", func_level=1, log_location="")
     else: print("Logging Error!")
 
 
+    
+
+
 
 # ----------------------------------------------------------------------------------------------------------------------
 
