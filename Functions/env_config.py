ebd01b05ec77d520ccf64a6eb8679a666b7c4487
RenacinM_Desktop <renacinm@gmail.com>
2026-05-30 14:33:49 -0400

    .

--- Functions/env_config.py
+++ Functions/env_config.py
@@ -16,7 +16,7 @@ class Config():
         self.BUS_LOC_URL         = r'https://gtfs-rt-merge.prod.bt-cadavl.com/BramptonTransit/GTFS/merged_VehiclePosition.json'
         self.GTFS_URL            = r'https://www.arcgis.com/sharing/rest/content/items/a355aabd5a8c490186bdce559c9c75fb/data'
         self.FOLDERs             = ['GTFS', 'BUS_STP', 'BUS_LOC', 'FRMTD_DATA', 'BUS_SPEED', 'GRAPHICS', 'LOGS']
-        self.GATHER_TABLE        = {"BUS_LOC_DB", "U_ID_TEMP", "ERROR_DB", "ROUTE_SPEED", "DB_LOGS", "DB_STATUS"}
+        self.GATHER_TABLE        = {"BUS_LOC_DB", "U_ID_TEMP", "ERROR_DB", "ROUTE_SPEED", "DB_LOGS"}
         self.table_dict          = {"BUS_LOC_DB":     ['u_id', 'id', 'trip_trip_id', 'trip_schedule_relationship', 'trip_route_id','position_latitude', 'position_longitude', 'position_bearing','position_speed', 'current_status', 'timestamp', 'stop_id','vehicle_id', 'vehicle_label', 'dt_colc'],
                                     "U_ID_TEMP":      ["u_id", "timestamp"],
                                     "ERROR_DB":       ["timestamp", "e_type", "delay"],
@@ -26,8 +26,7 @@ class Config():
                                     "STOPS":          ["stop_id", "stop_code", "stop_name", "stop_lat", "stop_lon", "zone_id", "stop_url", "parent_station", "feed_version"],
                                     "STOP_TIMES":     ["trip_id", "arrival_time", "departure_time", "stop_id", "stop_sequence", "pickup_type", "drop_off_type", "timepoint", "feed_version"],
                                     "ROUTE_SPEED":    ['trip_id', 'tot_dist', 'tot_idle_time', 'tot_trvl_time', 'tot_trip_time', 'avg_trip_speed', 'avg_trvl_speed', "feed_version"],
-                                    "DB_LOGS":        ['time_stamp', 'reporter', 'warning_level', 'info'],
-                                    "DB_STATUS":      ['time_stamp', 'status']
+                                    "DB_LOGS":        ['time_stamp', 'reporter', 'warning_level', 'info']
                                 }
 
         # Define All Needed Paths
