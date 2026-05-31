87182789493cd4e3ae29bce2397137ff48644995
RenacinM_Desktop <renacinm@gmail.com>
2026-05-31 11:12:35 -0400

    First thing the setup tool should do is create the log database so we can log everything that's happening to the main database without worries of locks

--- Functions/env_config.py
+++ Functions/env_config.py
@@ -17,6 +17,7 @@ class Config():
         self.GTFS_URL            = r'https://www.arcgis.com/sharing/rest/content/items/a355aabd5a8c490186bdce559c9c75fb/data'
         self.FOLDERs             = ['GTFS', 'BUS_STP', 'BUS_LOC', 'FRMTD_DATA', 'BUS_SPEED', 'GRAPHICS', 'LOGS']
         self.GATHER_TABLE        = {"BUS_LOC_DB", "U_ID_TEMP", "ERROR_DB", "ROUTE_SPEED", "DB_LOGS"}
+        self.log_dict            = {"DB_LOGS":        ['time_stamp', 'reporter', 'warning_level', 'info']}
         self.table_dict          = {"BUS_LOC_DB":     ['u_id', 'id', 'trip_trip_id', 'trip_schedule_relationship', 'trip_route_id','position_latitude', 'position_longitude', 'position_bearing','position_speed', 'current_status', 'timestamp', 'stop_id','vehicle_id', 'vehicle_label', 'dt_colc'],
                                     "U_ID_TEMP":      ["u_id", "timestamp"],
                                     "ERROR_DB":       ["timestamp", "e_type", "delay"],
@@ -26,8 +27,8 @@ class Config():
                                     "STOPS":          ["stop_id", "stop_code", "stop_name", "stop_lat", "stop_lon", "zone_id", "stop_url", "parent_station", "feed_version"],
                                     "STOP_TIMES":     ["trip_id", "arrival_time", "departure_time", "stop_id", "stop_sequence", "pickup_type", "drop_off_type", "timepoint", "feed_version"],
                                     "ROUTE_SPEED":    ['trip_id', 'tot_dist', 'tot_idle_time', 'tot_trvl_time', 'tot_trip_time', 'avg_trip_speed', 'avg_trvl_speed', "feed_version"],
-                                    "DB_LOGS":        ['time_stamp', 'reporter', 'warning_level', 'info']
-                                }
+                                    "DB_LOGS":        ['time_stamp', 'reporter', 'warning_level', 'info']}
+        
 
         # Define All Needed Paths
         self.cache_time_limit    = 5
@@ -37,6 +38,7 @@ class Config():
         db_out_path              = os.path.join(self.dwnld_path,         "BramptonTransitAnalysis", "3_Data")
         self.db_folder           = db_out_path
         self.db_path             = os.path.join(db_out_path,             "DataStorage.db")
+        self.dblog_path          = os.path.join(db_out_path,             "LogStorage.db")
         self.csv_out_path        = os.path.join(self.dwnld_path,         "BramptonTransitAnalysis", "4_Storage")
         self.rfresh_tkn_path     = os.path.join(self.dwnld_path,         "DropboxInfo", "GrabToken.sh")
         self.zip_path            = os.path.join(self.csv_out_path,       "GTFS", "GTFS.zip")
