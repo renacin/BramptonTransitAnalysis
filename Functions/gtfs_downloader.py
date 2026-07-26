52725f38d732c0f0cccc5a585bae648684686a4b
RenacinM_Desktop <renacinm@gmail.com>
2026-07-26 16:38:59 -0400

    Moved around the GTFS Downloader, things are more organized now

--- Functions/gtfs_downloader.py
+++ Functions/gtfs_downloader.py
@@ -31,10 +31,11 @@ class GTFS_Downloader():
     # ~~~~~~~~~~~~~~~~~~~~~ Public Function #1 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     def gather_GTFS(self):
 
-        """ Gather GTFS Data"""
+        """ Gather GTFS Data & Create Dependent Files"""
         self.__get_gtfs_data()
         self.__upld_gtfs_data()
         self.__create_routemasterkey()
+        self.__upld_trip_avg_speed()
 
 
 
@@ -71,21 +72,21 @@ class GTFS_Downloader():
             try:
                 # Extract Data
                 shutil.unpack_archive(self.cfg.zip_path, self.cfg.foldr_path)
-                shared_logger("Data Janitor  ", f"Checking For New GTFS Data", 1, self.cfg.dblog_path)
+                shared_logger("GTFS Checker", f"Checking For New GTFS Data", 1, self.cfg.dblog_path)
 
                 # Remove Unneeded Files & Folders
                 try:
                     os.remove(self.cfg.zip_path)
                 except OSError as e:
-                    shared_logger("Data Janitor  ", f"[ERROR] Could Not Remove Zip", 3, self.cfg.dblog_path)
+                    shared_logger("GTFS Checker", f"[ERROR] Could Not Remove Zip", 3, self.cfg.dblog_path)
                     raise e
             
             except shutil.ReadError as e:
-                shared_logger("Data Janitor  ", f"[ERROR] Could Not Extract GTFS Data", 3, self.cfg.dblog_path)
+                shared_logger("GTFS Checker", f"[ERROR] Could Not Extract GTFS Data", 3, self.cfg.dblog_path)
                 raise e
 
         else:
-            shared_logger("Data Janitor  ", f"[ERROR] Bad Response", 3, self.cfg.dblog_path)
+            shared_logger("GTFS Checker", f"[ERROR] Bad Response", 3, self.cfg.dblog_path)
             raise requests.exceptions.HTTPError(f"Bad response: {response.status_code}")
         
         
@@ -116,7 +117,7 @@ class GTFS_Downloader():
                             temp_df["feed_version"] = feed_cur_version
                             temp_df                 = temp_df[self.cfg.table_dict[file_name]]
                             temp_df.to_sql(file_name, conn, if_exists="append", index=False)
-                            shared_logger("Data Janitor  ", f"New GTFS Data Uploaded -> {file_name}", 1, self.cfg.dblog_path)
+                            shared_logger("GTFS Checker", f"New GTFS Data Uploaded -> {file_name}", 1, self.cfg.dblog_path)
 
 
                 # Delete All Text Files In Folder
@@ -221,7 +222,7 @@ class GTFS_Downloader():
                         # Export Data
                         out_file = os.path.join(out_path, f"{gtfs_feed_version}_ROUTEMASTERKEY.csv")
                         stops_df.to_csv(out_file, index=False)
-                        shared_logger("Data Janitor", f"New Route Masterkey Created: {gtfs_feed_version}", 1, self.cfg.dblog_path)
+                        shared_logger("GTFS Checker", f"New Route Masterkey Created: {gtfs_feed_version}", 1, self.cfg.dblog_path)
 
 
                 # If Something Happens Rollback To Begin, Inform User, And Wait
@@ -230,21 +231,121 @@ class GTFS_Downloader():
                         conn.execute("ROLLBACK")
                     except:
                         pass
-                    shared_logger("Data Janitor", f"Failed To Create New Route Masterkey: {e}", 2, self.cfg.dblog_path)
+                    shared_logger("GTFS Checker", f"Failed To Create New Route Masterkey: {e}", 2, self.cfg.dblog_path)
 
                 except KeyboardInterrupt:
                     try:
                         conn.execute("ROLLBACK")
                     except:
                         pass
-                    shared_logger("Data Janitor", f"Keyboard Interrupt", 2, self.cfg.dblog_path)
+                    shared_logger("GTFS Checker", f"Keyboard Interrupt", 2, self.cfg.dblog_path)
 
                 except Exception as e:
                     try:
                         conn.execute("ROLLBACK")
                     except:
                         pass
-                    shared_logger("Data Janitor", f"Failed To Create New Route Masterkey: {e}", 2, self.cfg.dblog_path)
+                    shared_logger("GTFS Checker", f"Failed To Create New Route Masterkey: {e}", 2, self.cfg.dblog_path)
+
+
+
+    # -------------------- Private Function #8 ---------------------------------
+    def __upld_trip_avg_speed(self):
+        """
+        This function will look at the GTFS Data pulled, if the feed version is new it will pull all stop locations and determine the 
+        lenght of the trip, and the average speed. The results will then be uploaded into a corresponding table in the database.
+        """
+
+        # Are There Different Feed Versions In The Data That We're Pulling?
+        with closing(sqlite3.connect(self.cfg.db_path)) as conn:
+            with conn:
+                max_colctd_feed_id     = pd.read_sql_query(""" SELECT DISTINCT 
+                                                                    MAX(feed_version) AS MAX_FEED_VER 
+                                                                FROM FEED_INFO """,   conn)['MAX_FEED_VER'].fillna(0).iloc[0]
+                
+                max_routes_feed_id     = pd.read_sql_query(""" SELECT DISTINCT 
+                                                                    MAX(feed_version) AS MAX_FEED_VER 
+                                                                FROM ROUTE_SPEED """, conn)['MAX_FEED_VER'].fillna(0).iloc[0]
+
+
+                # If No New Data Back Out
+                if int(max_colctd_feed_id) <= int(max_routes_feed_id):
+                    shared_logger("GTFS Checker", f"Speed Table Is Current", 1, self.cfg.dblog_path)
+                    return
+
+
+                # If New Data Present Calculate New Data Given New Feed Version
+                stops                           = pd.read_sql_query(f""" 
+                                                                        SELECT 
+                                                                            B.stop_id, 
+                                                                            B.stop_code, 
+                                                                            B.stop_name, 
+                                                                            B.stop_lat, 
+                                                                            B.stop_lon,  
+                                                                            B.feed_version
+                                                                        FROM STOPS      AS B  
+                                                                        WHERE B.feed_version = ? """, conn, params=(max_colctd_feed_id,))
+                
+                stops_times                     = pd.read_sql_query(f""" 
+                                                                        SELECT 
+                                                                            A.trip_id, 
+                                                                            A.stop_sequence, 
+                                                                            A.stop_id, 
+                                                                            A.arrival_time,  
+                                                                            A.departure_time, 
+                                                                            A.feed_version 
+                                                                        FROM STOP_TIMES AS A  
+                                                                        WHERE A.feed_version = ? """, conn, params=(max_colctd_feed_id,))
+                
+                stops_times["stop_id"]          = stops_times["stop_id"].astype(str).str.lstrip("0")
+                stops["stop_id"]                = stops["stop_id"].astype(str).str.lstrip("0")
+                stops_times["feed_version"]     = stops_times["feed_version"].astype(int)
+                stops["feed_version"]           = stops["feed_version"].astype(int)
+                stops_times                     = stops_times.merge(stops[["stop_id", "feed_version", "stop_code", "stop_name", "stop_lat", "stop_lon"]], on=["stop_id", "feed_version"], how="left").sort_values(["trip_id", "stop_sequence"])
+                del stops
+
+
+                # Create Lagged Columns & Determine Distance Between
+                shift_cols = ["stop_id", "stop_name", "stop_lat", "stop_lon", "arrival_time", "departure_time"]
+                shifted                  = stops_times.groupby("trip_id")[shift_cols].shift(-1).rename(columns={c: f"nxt_{c}" for c in shift_cols})
+                stops_times              = pd.concat([stops_times, shifted], axis=1)[['trip_id', 'stop_sequence', 'stop_id', 'arrival_time', 'departure_time', 'stop_code', 'stop_name', 'stop_lat', 'stop_lon', 'nxt_stop_id', 'nxt_stop_name', 'nxt_stop_lat', 'nxt_stop_lon', 'nxt_arrival_time', 'nxt_departure_time', 'feed_version']]
+                stops_times['km2nxtstp'] = hvrsn_dist((stops_times['stop_lat'].values, stops_times['stop_lon'].values), (stops_times['nxt_stop_lat'].values, stops_times['nxt_stop_lon'].values))
+
+
+                # Deal With The Missing Data At The End Of A Trip
+                for col in ["stop_lat", "stop_lon", "stop_name", "arrival_time", "departure_time"]:
+                    stops_times[f"nxt_{col}"] = stops_times[f"nxt_{col}"].fillna(stops_times[f"{col}"])
+                stops_times["km2nxtstp"] = stops_times["km2nxtstp"].fillna(0)
+
+
+                # Convert Time To Operable Time Stamp
+                for col in ["arrival_time", "departure_time", "nxt_arrival_time", "nxt_departure_time"]:
+                    stops_times[['h', 'm', 's']] = stops_times[col].str.split(':', expand=True)
+                    stops_times[['h', 'm', 's']] = stops_times[['h', 'm', 's']].astype(int)
+                    stops_times[f"{col}_sec"]    = (stops_times['h'] * 3600) + (stops_times['m'] * 60) + stops_times['s']
+                    stops_times.drop(columns     = ['h', 'm', 's'], inplace=True)
+
+
+                # Determine Time Between Arrival & Departure & Time To Next Stop
+                stops_times["idle_time"] = (stops_times["departure_time_sec"]   - stops_times["arrival_time_sec"])
+                stops_times["trvl_time"] = (stops_times["nxt_arrival_time_sec"] - stops_times["departure_time_sec"])
+                stops_times.drop(columns = ["arrival_time_sec", "departure_time_sec", "nxt_arrival_time_sec", "nxt_departure_time_sec"], inplace = True)
+
+
+                # Determine Total Travel Time, Idle Time, Average Speed For Trip, Average Speed For Section
+                avg_spd_df                     = stops_times.groupby(["trip_id", "feed_version"], as_index=False).agg(tot_dist = ("km2nxtstp", "sum"), tot_idle_time = ("idle_time", "sum"), tot_trvl_time = ("trvl_time", "sum"))
+                avg_spd_df["tot_trip_time"]    = avg_spd_df["tot_idle_time"] +  avg_spd_df["tot_trvl_time"]
+                avg_spd_df["avg_trip_speed"]   = (avg_spd_df["tot_dist"] / (avg_spd_df["tot_trip_time"] / 3600).replace(0, float("nan")))
+                avg_spd_df["avg_trvl_speed"]   = (avg_spd_df["tot_dist"] / (avg_spd_df["tot_trvl_time"] / 3600).replace(0, float("nan")))
+
+
+                # Round The Following Columns & Return Data
+                avg_spd_df[["tot_dist", "avg_trip_speed", "avg_trvl_speed"]] = avg_spd_df[["tot_dist", "avg_trip_speed", "avg_trvl_speed"]].round(2)
+            
+
+                # Upload The Speed Dataframe Data To Respective Table
+                avg_spd_df.to_sql("ROUTE_SPEED", conn, if_exists="append", index=False)
+                shared_logger("GTFS Checker", f"New Route Speed Data Uploaded", 1, self.cfg.dblog_path)
 
 
 
